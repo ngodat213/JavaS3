4b986edba793bfc8b259ceaa5675620856537091
Hydra Coder <96688421+ngodat213@users.noreply.github.com>
2024-06-11 20:24:47 +0700

    update

--- src/main/java/com/hutech/javas3d3/Repositories/ClassroomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hutech.javas3d3.Repositories;
-
-import com.hutech.javas3d3.Entities.Classroom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClassroomRepository extends JpaRepository<Classroom, String> {
-}
